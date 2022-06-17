15f405d5d1d8ff36212232212402745a19f52f40
macbeth <layne.wegenast@gmail.com>
2022-06-17 11:32:28 -0500

    started service worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -16,3 +16,49 @@ request.onupgradeneeded = function(event) {
   request.onerror = function(event) {
     console.log(event.target.errorCode);
   };
+
+  function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+  
+    budgetObjectStore.add(record);
+  }
+
+  function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+  
+    const getAll = budgetObjectStore.getAll();
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+          fetch('/api/', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(response => response.json())
+            .then(serverResponse => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+              }
+              const transaction = db.transaction(['new_transaction'], 'readwrite');
+              const budgetObjectStore = transaction.objectStore('new_transaction');
+              budgetObjectStore.clear();
+    
+              alert('All saved transactions have been submitted!');
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      };
+    
+  }
+
+  window.addEventListener('online', uploadTransaction);
