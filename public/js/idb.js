f3144a398b581143ca7cdd359dcdbaf1a0ca8855
macbeth <layne.wegenast@gmail.com>
2022-06-17 10:57:41 -0500

    working on offline functionality

---
+++ public/js/idb.js
@@ -0,0 +1,18 @@
+let db;
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+  };
+
+  request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+  };
+  
+  request.onerror = function(event) {
+    console.log(event.target.errorCode);
+  };
