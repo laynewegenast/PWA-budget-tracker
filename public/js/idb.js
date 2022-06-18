747a04fec1648b047a4ba3b7e570ecbd9fce3070
macbeth <layne.wegenast@gmail.com>
2022-06-18 10:44:16 -0500

    added bulk in idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -34,7 +34,7 @@ request.onupgradeneeded = function(event) {
     getAll.onsuccess = function() {
         // if there was data in indexedDb's store, let's send it to the api server
         if (getAll.result.length > 0) {
-          fetch('/api/', {
+          fetch('/api/transaction/bulk', {
             method: 'POST',
             body: JSON.stringify(getAll.result),
             headers: {
